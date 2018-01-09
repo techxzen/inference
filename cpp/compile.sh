2788f5592b6160e3aedeb3d7d97f3abf20429cd4
itchencheng <itchencheng@gmail.com>
2018-01-09 23:48:38 +0800

    add code

--- cpp/compile.sh
+++ cpp/compile.sh
@@ -5,5 +5,7 @@ g++  -o inference inference.cpp \
     -I $CAFFE_HOME/.build_release/src/ \
     -L $CAFFE_HOME/build/lib/ \
     -lcaffe \
-    -lboost_system
-    
+    -lboost_system \
+    -lopencv_highgui \
+    -lopencv_imgproc \
+    -lopencv_core
---
+++ cpp/inference.cpp
@@ -0,0 +1,65 @@
+//An Inference programming
+//Author: chencheng
+//Date:   20180108
+
+#include<iostream>
+#include<string>
+#include<caffe/caffe.hpp>
+
+#include<opencv2/core/core.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+#include<opencv2/highgui/highgui.hpp>
+
+
+using namespace std;
+using namespace caffe;
+
+//usage: inference model.prototxt model.caffemodel label.txt image.jpg
+
+int main(int argc, char **argv)
+{
+    if(5 != argc)
+    {
+        cerr << "Usage: " << argv[0]
+              << " deploy.prototxt network.caffemodel"
+              << " mean.binaryproto labels.txt img.jpg" << std::endl;
+        return 1;
+    }
+
+
+    string model_file;
+    string weight_file;
+    string label_file;
+    string image_file;
+
+
+    //create Net
+    Net<float>* net;
+    net  = new Net<float>(model_file, caffe::TEST);
+    
+    //copy weight
+    net -> CopyTrainedLayersFrom(weight_file);
+
+    //get input blob
+    Blob<float>* input_blob = net->input_blobs()[0];
+    
+    int num_channels;
+    int num_width;
+    int num_height;
+
+    //read image file
+    cv::Mat img = cv::imread(image_file, -1);
+
+    
+
+    //get output blob
+    Blob<float>* output_blob = net->output_blobs()[0];
+    
+
+    
+    
+    
+
+    cout << "inference complete!" <<endl;
+    return 0; 
+}
